62d68670b8c3a5acabbea1cd8062646804f3c57b
Nitintembhare29 <115885821+Nitintembhare29@users.noreply.github.com>
2023-04-21 17:27:00 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,1 +1,29 @@
 //your JS code here. If required.
+function login() {
+			var username = document.getElementById("username").value;
+			var password = document.getElementById("password").value;
+			var remember = document.getElementById("checkbox").checked;
+
+			if (remember) {
+				localStorage.setItem("username", username);
+				localStorage.setItem("password", password);
+			} else {
+				localStorage.removeItem("username");
+				localStorage.removeItem("password");
+			}
+
+			alert("Logged in as " + username);
+		}
+
+		window.onload = function() {
+			var username = localStorage.getItem("username");
+			var existingButton = document.getElementById("existing");
+
+			if (username) {
+				existingButton.style.display = "inline";
+			}
+
+			existingButton.onclick = function() {
+				alert("Logged in as " + username);
+			};
+		};
